c9807ac3fcf166a8d2d333811c58cd936cb6b86f
tjenkins <torii.j.jenkins@gmail.com>
2020-11-28 16:01:03 -0500

    Setup Restaurant Index View

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RestaurantRater.Models;
+using static RestaurantRater.Models.Restaurant;
 
 namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+
         // GET: Restaurant/Index
         public ActionResult Index()
         {
-            return View();
+            return View (_db.Restaraunts.ToList());
         }
     }
 }
