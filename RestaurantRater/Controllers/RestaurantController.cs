bf3a04df63206bf935cefdf338a5b2c6ff1e1ce7
tjenkins <torii.j.jenkins@gmail.com>
2020-11-28 17:55:57 -0500

    Added Create Controller and View

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -11,11 +11,35 @@ namespace RestaurantRater.Controllers
     public class RestaurantController : Controller
     {
         private RestaurantDbContext _db = new RestaurantDbContext();
-
         // GET: Restaurant/Index
         public ActionResult Index()
         {
             return View (_db.Restaraunts.ToList());
         }
+
+        //GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        //POST: RestaurantCreate
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create (Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaraunts.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+
     }
+
+    
 }
